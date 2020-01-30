dde46c1fe5bc30c62823e5dd160afe5081204fa2
Jodi N Kansagor <joedvice@gmail.com>
2020-01-30 13:48:54 -0800

    buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button =({})
+const Button = ({ onSubmit, buttonName}) => (
+<button onSubmit={onSubmit}>{buttonName}</button>
+);
+
+Button.PropTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  button
+}
